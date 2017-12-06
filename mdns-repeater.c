3fc8049f4672ed9512b3446fc8a35440e242dcf9
Christian Poessinger <christian@poessinger.com>
2017-12-06 17:21:15 +0100

    Remove trailing whitespaces

--- mdns-repeater.c
+++ mdns-repeater.c
@@ -1,17 +1,17 @@
 /*
  * mdns-repeater.c - mDNS repeater daemon
  * Copyright (C) 2011 Darell Tan
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -217,7 +217,7 @@ static pid_t already_running() {
 	FILE *f;
 	int count;
 	pid_t pid;
-   
+
 	f = fopen(pid_file, "r");
 	if (f != NULL) {
 		count = fscanf(f, "%d", &pid);
@@ -414,7 +414,7 @@ int main(int argc, char *argv[]) {
 			struct sockaddr_in fromaddr;
 			socklen_t sockaddr_size = sizeof(struct sockaddr_in);
 
-			ssize_t recvsize = recvfrom(server_sockfd, pkt_data, PACKET_SIZE, 0, 
+			ssize_t recvsize = recvfrom(server_sockfd, pkt_data, PACKET_SIZE, 0,
 				(struct sockaddr *) &fromaddr, &sockaddr_size);
 			if (recvsize < 0) {
 				log_message(LOG_ERR, "recv(): %m");
@@ -461,14 +461,14 @@ int main(int argc, char *argv[]) {
 
 end_main:
 
-	if (pkt_data != NULL) 
+	if (pkt_data != NULL)
 		free(pkt_data);
 
 	if (server_sockfd >= 0)
 		close(server_sockfd);
 
 	if (socks != NULL) {
-		for (i = 0; i < num_socks; i++) 
+		for (i = 0; i < num_socks; i++)
 			close(socks[i].sockfd);
 
 		free(socks);
